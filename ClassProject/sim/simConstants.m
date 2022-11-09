d9316c2bc7a177784e12987692292a75278b6b80
Logan Dihel <logan.dihel@gmail.com>
2022-11-09 00:15:14 -0800

    Added LQR and realistic quantization

--- ClassProject/sim/simConstants.m
+++ ClassProject/sim/simConstants.m
@@ -12,3 +12,10 @@ Ts = 50e-6;
 
 % initial condition
 theta0 = deg2rad(5);
+
+% lqr control
+A = [0 1 0 0; 0 0 0 0; 0 0 0 1; 0 0 g/L 0];
+B = [0; 1; 0; -1/L];
+Q = diag([1 1 10 0]);
+R = 100;
+K = lqr(A,B,Q,R,0);
